c3bacdfc09c37e01d09144d2ab1202debac139b8
Jose Leiva <email.joseleiva@gmail.com>
2025-05-31 16:02:00 -0400

    Add microservices support to the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,30 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
+  const SERVER_PORT: number = Number(process.env.SERVER_PORT ?? 3000);
+  const IA_SERVICE_PORT: number = Number(process.env.IA_SERVICE_PORT ?? 3001);
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.TCP,
+      options: {
+        port: IA_SERVICE_PORT,
+      },
+    },
+    // to use main app global pipes
+    // {
+    //   inheritAppConfig: true,
+    // },
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -18,8 +36,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  const PORT = process.env.PORT ?? 3000;
-  console.log(`[Nest] port at: http://localhost:${PORT}/`);
-  await app.listen(PORT);
+
+  console.log(`[Nest] port at: http://localhost:${SERVER_PORT}/`);
+
+  await app.startAllMicroservices();
+  await app.listen(SERVER_PORT);
 }
+
 void bootstrap();
